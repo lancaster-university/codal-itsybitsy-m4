7cc8933f676663ccec29a12615f4850a6442d079
Michal Moskal <michal@moskal.me>
2018-09-20 23:26:26 -0700

    It blinks!

--- src/init.cpp
+++ src/init.cpp
@@ -2,13 +2,21 @@
 #include "codal_target_hal.h"
 #include "CodalDmesg.h"
 
+
+extern "C" {
+#include "clocks.h"
+}
+
 void target_init();
 
 extern "C" void cpu_init()
 {
     //SystemCoreClockUpdate();
 
+    clock_init();
+
     target_init();
+
     // TODO
 
 }
