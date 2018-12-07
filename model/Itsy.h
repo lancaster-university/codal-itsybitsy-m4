5ebcfc6a4a86b7b0e5c02bb9649e0b7f36ab285f
Michal Moskal <michal@moskal.me>
2018-12-07 15:27:46 -0800

    Make sure we build against the samd21 branch of codal-samd

--- model/Itsy.h
+++ model/Itsy.h
@@ -29,7 +29,6 @@ DEALINGS IN THE SOFTWARE.
 #include "codal-core/inc/types/Event.h"
 #include "CodalDevice.h"
 #include "ErrorNo.h"
-#include "ZTimer.h"
 #include "CodalCompat.h"
 #include "CodalComponent.h"
 #include "CodalDmesg.h"
